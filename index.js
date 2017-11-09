10fe724e4319233d99766e39fcfb54e7f98b5b73
Christine Shaffer <cmc.shaffer@gmail.com>
2017-11-09 17:29:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string name) {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
 }
